b137411b4ff4a96484305b9c29ee5596bde10e41
Mario Mastrandrea <m.mastrandrea@reply.it>
2024-06-18 17:26:14 +0200

    Removed unnecessary clientError

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -54,7 +54,6 @@ final class RemoteFeedLoaderTests: XCTestCase {
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
         
-        let clientError = NSError(domain: "Test", code: 0)
         client.complete(withStatusCode: 400)
         
         XCTAssertEqual(capturedErrors, [.invalidData])
