09c63b81161477ab262582331f67b153639dd7ef
Mario Mastrandrea <m.mastrandrea@reply.it>
2024-06-18 15:13:08 +0200

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Mario Mastrandrea on 18/06/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
