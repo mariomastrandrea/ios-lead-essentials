ca7994998a7f76c1320246bf298c6a1115a030ba
Mario Mastrandrea <m.mastrandrea@reply.it>
2024-06-20 16:44:11 +0200

    Move the URLSessionHTTPClient (the system under test, or "SUT") creation to a factory method to protect out tests from breaking changes

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -47,7 +47,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -57,11 +57,9 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = URLSessionHTTPClient()
-            
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 Self.assertEqualNSErrors(receivedError, error)
@@ -76,6 +74,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private static func assertEqualNSErrors(_ err1: NSError, _ err2: NSError, file: StaticString = #filePath, line: UInt = #line) {
         XCTAssertEqual(err1.domain, err2.domain, file: file, line: line)
